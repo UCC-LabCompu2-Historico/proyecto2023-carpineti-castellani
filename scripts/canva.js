f08fed3ac990615ea4ee9e60586d050a4b807d80
Franco Castellani <114883365+francocaste2705@users.noreply.github.com>
2023-06-04 23:34:41 -0300

    mod 2

--- scripts/canva.js
+++ scripts/canva.js
@@ -1,16 +1,16 @@
-const canvas = document.getElementById("pixel-canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('pixel-canvas');
+const ctx = canvas.getContext('2d');
 const pixelSize = 10;
 const canvasSize = 20;
 
 // Configuración inicial del lienzo
 canvas.width = canvasSize * pixelSize;
 canvas.height = canvasSize * pixelSize;
-ctx.fillStyle = "#ffffff";
+ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Variables para el color y pincel seleccionado
-let selectedColor = "#000000";
+let selectedColor = '#000000';
 let isErasing = false;
 
 // Función para dibujar un píxel
@@ -28,7 +28,7 @@ function getCursorPosition(event) {
 }
 
 // Función para dibujar en el lienzo al hacer clic
-canvas.addEventListener("click", (event) => {
+canvas.addEventListener('click', (event) => {
     const { x, y } = getCursorPosition(event);
     if (isErasing) {
         ctx.clearRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
@@ -49,14 +49,14 @@ function erase() {
 }
 
 // Cambiar el color seleccionado al hacer clic en un botón de color
-const colorButtons = document.querySelectorAll(".color-btn");
+const colorButtons = document.querySelectorAll('.color-btn');
 colorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
         const color = button.dataset.color;
         changeColor(color);
     });
 });
 
 // Borrar en el lienzo al hacer clic en el botón de borrar
-const eraseButton = document.getElementById("erase-btn");
-eraseButton.addEventListener("click", erase);
+const eraseButton = document.getElementById('erase-btn');
+eraseButton.addEventListener('click', erase);
